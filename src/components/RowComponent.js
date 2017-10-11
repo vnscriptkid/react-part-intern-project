97fd0d625e2b36e79697db5da1eaa8d613248009
vnscriptkid <vnscriptkid@gmail.com>
2017-10-12 02:31:51 +0700

    add font color

--- src/components/RowComponent.js
+++ src/components/RowComponent.js
@@ -3,17 +3,33 @@ import {
   TableRowColumn,
   TableRow  
 } from 'material-ui/Table';
- 
+
+function pickColor(num){
+	return num >= 0 ? {color:'#41f47d'} : {color:'#f44242'};
+}
+
 class RowComponent extends Component {
 	render(){		
 		return (						
 			<TableRow>
-				<TableRowColumn>{this.props.stock.code}</TableRowColumn>
+				<TableRowColumn 
+					style={{color:'#03A9F4'}}
+				>
+					{this.props.stock.code}
+				</TableRowColumn>
 				<TableRowColumn>{this.props.stock.company}</TableRowColumn>
 				<TableRowColumn>{this.props.stock.originalPrice}</TableRowColumn>
 				<TableRowColumn>{this.props.stock.value}</TableRowColumn>
-				<TableRowColumn>{this.props.stock.priceChange}</TableRowColumn>
-				<TableRowColumn>{this.props.stock.priceChangePercent} %</TableRowColumn>
+				<TableRowColumn
+					style={pickColor(this.props.stock.priceChange)}
+				>
+					{this.props.stock.priceChange}
+				</TableRowColumn>
+				<TableRowColumn
+					style={pickColor(this.props.stock.priceChangePercent)}
+				>
+					{this.props.stock.priceChangePercent} %
+				</TableRowColumn>
 			</TableRow>						
 		)
 	}	 	
